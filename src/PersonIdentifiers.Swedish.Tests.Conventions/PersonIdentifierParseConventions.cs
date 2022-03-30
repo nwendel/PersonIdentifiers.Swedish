72050ecef0436b17c981a491387252eb2ebdec74
niklaswendel <niklas.wendel@gmail.se>
2022-03-30 06:10:22 +0200

    Added convention for static parse method

---
+++ src/PersonIdentifiers.Swedish.Tests.Conventions/PersonIdentifierParseConventions.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using PersonIdentifiers.Swedish.Tests.Conventions.TestHelpers;
+using Xunit;
+
+namespace PersonIdentifiers.Swedish.Tests.Conventions;
+
+public class PersonIdentifierParseConventions
+{
+    [Fact]
+    public void HasParseMethod()
+    {
+        var assembly = typeof(PersonIdentifier).Assembly;
+        var types = assembly.GetTypes()
+            .Where(x => x.IsAssignableTo(typeof(PersonIdentifier)))
+            .ToList();
+
+        foreach (var type in types)
+        {
+            var method = type.GetMethod(nameof(PersonIdentifier.Parse), System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static, new[] { typeof(string) });
+            if (method == null)
+            {
+                throw new ConventionException($"Type {type.Name} does not have a public static method named {nameof(PersonIdentifier.Parse)}");
+            }
+
+            if (method.ReturnType != type)
+            {
+                throw new ConventionException($"Type {type.Name} method {nameof(PersonIdentifier.Parse)} does not return type {type.Name}");
+            }
+        }
+    }
+}
---
+++ src/PersonIdentifiers.Swedish.Tests.Conventions/TestHelpers/ConventionException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PersonIdentifiers.Swedish.Tests.Conventions.TestHelpers;
+
+public class ConventionException : Exception
+{
+    public ConventionException(string message)
+        : base(message)
+    {
+    }
+}
