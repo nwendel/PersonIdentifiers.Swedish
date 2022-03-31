b164e1cbad17f77efd351e6ef613b93a5abdac39
niklaswendel <niklas.wendel@gmail.se>
2022-03-31 18:01:06 +0200

    Added blacklist texts for NRID

---
+++ src/PersonIdentifiers.Swedish/NationalReserveNumberIdentifierParts.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonIdentifiers.Swedish;
+
+public class NationalReserveNumberIdentifierParts : StandardPersonIdentifierParts
+{
+    public NationalReserveNumberIdentifierParts(string value)
+        : base(value)
+    {
+        SequenceAndGender = value[8..11];
+    }
+
+    // TODO: This should fail the convention unit tests
+    public string SequenceAndGender { get; }
+}
