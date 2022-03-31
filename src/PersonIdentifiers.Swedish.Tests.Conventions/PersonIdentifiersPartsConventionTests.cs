9527125bc998ab5fcfc66514977af7a83ebfe004
niklaswendel <niklas.wendel@gmail.se>
2022-03-31 08:04:59 +0200

    Added convention unit test for enumerating parts

---
+++ src/PersonIdentifiers.Swedish.Tests.Conventions/PersonIdentifiersPartsConventionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PersonIdentifiers.Swedish.Tests.Conventions.TestHelpers;
+using PersonIdentifiers.Swedish.Tests.Conventions.TestHelpers.Conventions;
+using Xunit;
+
+namespace PersonIdentifiers.Swedish.Tests.Conventions;
+
+public class PersonIdentifiersPartsConventionTests
+{
+    private static readonly IEnumerable<Type> _personIdentifierPartsTypes =
+        typeof(PersonIdentifierParts).Assembly
+        .GetTypes()
+        .Where(x => x.IsAssignableTo(typeof(PersonIdentifierParts)) &&
+                    !x.IsAbstract)
+        .ToList();
+
+    [Fact]
+    public void CanEnumerateParts()
+    {
+        ConventionAssert.TypesFollow<PersonIdentifierPartTypesMustEnumerateAllProperties>(_personIdentifierPartsTypes);
+    }
+}
---
+++ src/PersonIdentifiers.Swedish.Tests.Conventions/TestHelpers/Conventions/PersonIdentifierPartTypesMustEnumerateAllProperties.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PersonIdentifiers.Swedish.Internal;
+
+namespace PersonIdentifiers.Swedish.Tests.Conventions.TestHelpers.Conventions;
+
+public class PersonIdentifierPartTypesMustEnumerateAllProperties : TypeConvention
+{
+    public override void Assert(Type type)
+    {
+        GuardAgainst.Null(type);
+
+        var instance = (IEnumerable<(string Name, object Value)>?)Activator.CreateInstance(type, "191212121212");
+        if (instance == null)
+        {
+            throw new InvalidOperationException("No instance created");
+        }
+
+        var properties = type.GetProperties();
+
+        var elements = instance
+            .Zip(properties, (part, property) => (Part: part, Property: property))
+            .ToList();
+        foreach (var element in elements)
+        {
+            var elementName = element.Property.Name;
+            if (elementName != element.Part.Name)
+            {
+                var partNames = elements
+                    .Select(x => x.Part.Name)
+                    .ToList();
+                if (partNames.Contains(elementName))
+                {
+                    Fail(type, $"must return part named {elementName} in correct sequence");
+                }
+
+                Fail(type, $"must return part named {elementName}");
+            }
+        }
+    }
+}
