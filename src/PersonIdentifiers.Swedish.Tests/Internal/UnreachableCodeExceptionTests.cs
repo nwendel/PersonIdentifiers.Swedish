972713a7ff2f7908420a40760f4c0b50494bccfd
niklaswendel <niklas.wendel@gmail.se>
2022-04-02 15:26:10 +0200

    More unit tests

---
+++ src/PersonIdentifiers.Swedish.Tests/Internal/UnreachableCodeExceptionTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using PersonIdentifiers.Swedish.Internal;
+using Xunit;
+
+namespace PersonIdentifiers.Swedish.Tests.Internal
+{
+    public class UnreachableCodeExceptionTests
+    {
+        [Fact]
+        public void CanCreateWithMessage()
+        {
+            var ex = new UnreachableCodeException("the-message");
+            Assert.Equal("the-message", ex.Message);
+        }
+
+        [Fact]
+        public void ThrowsOnCreateNullMessage()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new UnreachableCodeException(null!));
+            Assert.Equal("message", ex.ParamName);
+        }
+    }
+}
